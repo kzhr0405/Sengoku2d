742c343f31897130813d626d6195f66daa170fe3
yushi_jack <ihsuyadihcu@gmail.com>
2017-05-26 23:09:46 +0900

    pvp合戦から戻るとBGMが消える不具合の修正(合わせてSEControllerが増えていくのにも対処)

---
+++ Assets/Scripts/OnlyOneExistBGMController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyOneExistBGMController : MonoBehaviour{
+    void Awake()
+    {
+        if (FindObjectsOfType<OnlyOneExistBGMController>().Length >= 2)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/OnlyOneExistSEController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyOneExistSEController : MonoBehaviour {
+    void Awake()
+    {
+        if (FindObjectsOfType<OnlyOneExistSEController>().Length >= 2)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
